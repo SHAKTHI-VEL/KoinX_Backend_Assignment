b8550d82ab200e7b0ab3c57f1baf8d7b0bd4212e
SHAKTHI-VEL <shakthivel28203@gmail.com>
2024-10-11 15:44:16 +0530

    Error handled for data not found and decimal rounded upto 2 places

--- controller/fetchDeviation.js
+++ controller/fetchDeviation.js
@@ -5,7 +5,11 @@ const fetchDeviation=async(req,res)=>{
     try {
         const {coin}=req.body;
         const data=await Coin.find({coinName:coin}).sort({ createdAt: -1 }).limit(100);
-        return res.status(200).json({success:true,message:"Fetched data successfully",deviation:deviation(data)})
+        if(data.length==0){
+            return res.status(404).json({success:false,message:"Coin not found"})
+        }else{
+        return res.status(200).json({success:true,message:"Fetched data successfully",deviation:deviation(data).toFixed(2)})
+        }
     } catch (error) {
         return res.status(500).json({success:false,message:"Internal Server Error"})
     }
--- controller/fetchStats.js
+++ controller/fetchStats.js
@@ -4,6 +4,9 @@ const fetchStats=async(req,res)=>{
     try {
         const {coin}=req.body;
         const data=await Coin.findOne({coinName:coin}).sort({ createdAt: -1 });
+        if(!data){
+            return res.status(404).json({success:false,message:"Coin not found"})
+        }
         return res.status(200).json({success:true,message:"Fetched data successfully",data:{
             "price":data.price,
             "marketCap":data.marketCapital,
